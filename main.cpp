896bfd5cee14230c6b588c96a708de2b47118c0f
a@a.a <a@a.a>
2023-11-22 15:25:36 +0100

    create deque of point class

--- main.cpp
+++ main.cpp
@@ -4,38 +4,93 @@
 // 1 comprendre deque
 // 2 créer double deque
 
+class Point
+{
+public:
+	Point(int _x, int _y)
+	{
+		x = _x;
+		y = _y;
+	}
+	~Point()
+	{
+	}
+	const int& getX() const
+	{
+		return x;
+	}
+	const int& getY() const
+	{
+		return y;
+	}
+	void setX(int _x)
+	{
+		x = _x;
+	}
+	void setY(int _y)
+	{
+		y = _y;
+	}
+private:
+	int x;
+	int y;
+};
+
 class Map
 {
 public:
-	Map()
+	Map(int _xSize, int _ySize)
 	{
+		xSize = _xSize;
+		ySize = _ySize;
+
+		for (int i = 0; i != ySize; ++i)
+		{
+			createLine(_xSize, i);
+		}
 	}
 	~Map()
 	{
 	}
-	const std::deque<std::deque<int>> &getBoard() const
+	
+	const std::deque<std::deque<Point>> &getBoard() const
 	{
 		return board;
 	}
 
-private:
-	std::deque<std::deque<int>> board;
-};
-
-void displayMap(const std::deque<std::deque<int>> &deque)
-{
-	for (auto line : deque)
+	void displayMap()
 	{
-		for (auto cell : line)
+		for (auto line : board)
 		{
-			std::cout << cell;
+			for (auto cell : line)
+			{
+				std::cout << "{" << cell.getY() << "," << cell.getX() << "},";
+			}
+			std::cout << std::endl;
 		}
-		std::cout << std::endl;
 	}
-}
+
+private:
+	int xSize;
+	int ySize;
+	std::deque<std::deque<Point>> board;
+
+	void createLine(int lineSize, int y)
+	{
+		std::deque<Point> line;
+
+		for (int i = 0; i != lineSize; ++i)
+		{
+			line.push_back(Point(i, y));
+		}
+			
+		board.push_back(line);
+	}
+};
 
 int main()
 {
-	Map map();
-	return 1;
+	Map map(5, 5);
+	map.displayMap();
+	return 0;
 }
