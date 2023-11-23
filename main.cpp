8f3dac667d329af415308c691ed730944927a4bd
a@a.a <a@a.a>
2023-11-23 14:48:53 +0100

    fix generation by point bug

--- main.cpp
+++ main.cpp
@@ -6,59 +6,15 @@
 
 int main()
 {
-	Map map(3, 2);
-	map.createNewColumn(COORDINATE_DIRECTION::POSITIVE);
-	map.createNewColumn(COORDINATE_DIRECTION::NEGATIVE);
-	map.createNewColumn(COORDINATE_DIRECTION::POSITIVE);
-	map.createNewColumn(COORDINATE_DIRECTION::NEGATIVE);
-	map.createNewColumn(COORDINATE_DIRECTION::POSITIVE);
-	map.createNewColumn(COORDINATE_DIRECTION::NEGATIVE);
-	map.displayMap();
-	std::cout << "-----" << std::endl;
-	map.createNewLine(COORDINATE_DIRECTION::POSITIVE);
-	map.displayMap();
-	std::cout << "-----" << std::endl;
-	map.createNewLine(COORDINATE_DIRECTION::NEGATIVE);
-	map.displayMap();
+	Map map(0, 0);
 
-	std::cout << "-----" << std::endl;
-	map.createNewLine(COORDINATE_DIRECTION::NEGATIVE);
-	map.displayMap();
+	map.createNewPoint(12, 12);
+	map.createNewPoint(-12, 12);
+	map.createNewPoint(12, -12);
+	map.createNewPoint(-12, -12);
 
-	std::cout << "-----" << std::endl;
-	map.createNewLine(COORDINATE_DIRECTION::POSITIVE);
-	map.displayMap();
-
-
-	map.createNewPoint(-7, 9);
-	std::cout << "###-----###" << std::endl;
-	map.displayMap();
-
-	map.createNewPoint(-8, 10);
 	std::cout << "##########" << std::endl;
 	map.displayMap();
-
-	map.createNewPoint(6, 11);
-	std::cout << "##########" << std::endl;
-	map.displayMap();
-
-	map.createNewPoint(-9, -3);
-	std::cout << "##########" << std::endl;
-	map.displayMap();
-
-	map.createNewPoint(7, -4);
-	std::cout << "##########" << std::endl;
-	map.displayMap();
-
-	map.createNewPoint(7, -4);
-	std::cout << "##########" << std::endl;
-	//map.displayMap();
-
-	map.createNewPoint(3, 3);
-	std::cout << "##########" << std::endl;
-
-	map.createNewPoint(20, 20);
-	std::cout << "##########" << std::endl;
 
 	return 0;
 }
