6b734e2237a246f4c637f95f451220d55fdb2b88
Khaarn <shrike3emecompagnie@gmail.com>
2023-11-23 12:08:07 +0100

    add noeud files with declaration of functionnality

---
+++ noeud.cpp
@@ -0,0 +1,52 @@
+#include <math.h>
+/*
+class Noeud
+{
+	void getCoutPointDepartNoeudSuivant()
+	{
+
+	}
+	void getCoutPointDepartNoeudSuivant()
+	{
+
+	}
+	void getCoutPointDepartNoeudSuivant()
+	{
+
+	}
+	void setCoutPointDepartNoeudSuivant()
+	{
+
+	}
+	void setCoutPointDepartNoeudSuivant()
+	{
+
+	}
+	void setCoutPointDepartNoeudSuivant()
+	{
+
+	}
+	float distance(int x1, int y1, int x2, int y2) {
+		/* distance euclidienne 
+		return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	}
+
+	float calculcoutPointDepartNoeudSuivant()
+	{
+		return coutPointDepartNoeudSuivant;
+	}
+
+	float calculcoutNoeudSuivantPointArrive(int x1, int y1, int x2, int y2)
+	{
+		return distance(x1,y1,x2,y2);
+	}
+
+	void calculCoutSomme(float coutPointDepartNoeudSuivant, float coutNoeudSuivantPointArrive)
+	{
+		coutSomme = coutPointDepartNoeudSuivant + coutNoeudSuivantPointArrive;
+	}
+private:
+	float coutPointDepartNoeudSuivant;
+	float coutNoeudSuivantPointArrive;
+	float coutSomme;
+}; */
